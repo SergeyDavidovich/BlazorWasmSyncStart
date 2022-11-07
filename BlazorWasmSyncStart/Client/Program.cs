76e9e574623506d9d46450cfaef35d7fa1588a59
Sergiusz Kriwobokow <writesd@hotmail.com>
2022-11-07 16:07:21 +0100

    no comments

--- BlazorWasmSyncStart/Client/Program.cs
+++ BlazorWasmSyncStart/Client/Program.cs
@@ -19,7 +19,9 @@ namespace BlazorWasmSyncStart
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
+            //builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
+            builder.Services.AddSyncfusionBlazor();
+
 
             await builder.Build().RunAsync();
         }
